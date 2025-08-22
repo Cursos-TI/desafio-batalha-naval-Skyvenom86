caff52246390fd61308eadca4da6b959788f2d73
Skyvenom86 <95256852+Skyvenom86@users.noreply.github.com>
2025-08-22 20:51:11 -0300

    batalhaNaval.c skyvenom86

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,36 +5,25 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+     // Tabuleiro 5x5 para simplificar 
+    int tabuleiro[5] [5] = {0};
+     // Navio vertical (3 posições)
+    int inicioX = 1, inicioY = 2; // posição inicial (linha 1, coluna 2)
+    for (int i = 0; i < 3;i++) {
+        tabuleiro[inicioX + i][inicioY] = 1;
+    }
+    // Navio horizontal (4 posições)
+    int inicioX2 = 4, inicioY2 = 0; // posição inicial (linha 4, coluna 0)
+    for (int j = 0; j < 4; j++){
+        tabuleiro[inicioX2] [inicioY2 + j] = 1;
+    }
+    // Exibir as Coordenadas ocupadas pelos navios: \n");
+    for (int i=0; i < 5; i++) {
+        for (int j=0; j < 5; j++) {
+            if (Tabuleiro[i] [j] == 1) {
+                printf("(%d, %d)\n", i , j);
+                        }
+                 }
+    }
     return 0;
 }
